bf0bb063e8057a3db5e5c1d80074fe2267926d91
Micheal Pearce <michealpearce@outlook.com>
2021-06-20 15:23:32 -0400

    remove data pass test, add routes

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import _ from 'lodash'
 import api from '@api/index'
 import session from './middleware/session'
+import routes from './routes'
 
 const { loadNuxt, build } = require('nuxt')
 
@@ -15,12 +16,7 @@ export default loadNuxt(dev ? 'dev' : 'start').then(async nuxt => {
 
 	render.use(session)
 	render.use('/api', api)
-
-	render.use((req, res, next) => {
-		;(req.session as any).test = Math.random()
-		next()
-	})
-
+	render.use(routes)
 	render.use(nuxt.render)
 
 	const server = http.createServer(render)
---
+++ server/routes.ts
@@ -0,0 +1,4 @@
+import { Router } from 'express'
+
+const routes = Router()
+export default routes
