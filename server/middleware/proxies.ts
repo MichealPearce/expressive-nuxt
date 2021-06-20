c5b3c4ff7de8bf203bdd604e7775e36d9554c075
Micheal Pearce <michealpearce@outlook.com>
2021-06-20 01:33:00 -0400

    linted

--- server/middleware/proxies.ts
+++ server/middleware/proxies.ts
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 
 export default function proxies(app: Express) {
 	const FrontEndProxy = createProxyMiddleware({
-		target: process.env.CLIENT_URL,
+		target: process.env.CLIENT_URL
 	})
 
 	app.use(FrontEndProxy)
