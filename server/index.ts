b5890faadf7af1c23c536c126b5d0ea8fc4f0c3f
Micheal Pearce <michealpearce@outlook.com>
2021-06-20 15:56:45 -0400

    add comments

--- server/index.ts
+++ server/index.ts
@@ -7,15 +7,22 @@ import routes from './routes'
 
 const { loadNuxt, build } = require('nuxt')
 
+// Nuxt's render (using Connect) seems to ignore or override any handlers added to it so we create our own instance
 const render = express()
 const { APP_HOST, APP_PORT, NODE_ENV } = process.env
 const dev = NODE_ENV !== 'production'
 
+// start up Nuxt
 export default loadNuxt(dev ? 'dev' : 'start').then(async nuxt => {
 	if (dev) await build(nuxt)
 
+	// attach basic middleware used in all routes
 	render.use(session)
+
+	// attach the api routes
 	render.use('/api', api)
+
+	// attach developer defined routes used for modifying or piping data to Nuxt
 	render.use(routes)
 	render.use(nuxt.render)
 
